b870a527892b6d00583baafc95fc0d4fa6a88ce0
aghababaei-alireza <aghababaei1375@gmail.com>
2023-10-01 15:18:12 +0330

    2023-10-01-W

--- members/views.py
+++ members/views.py
@@ -24,7 +24,7 @@ def main(request):
     return HttpResponse(template.render())
 
 def testing(request):
-    mymembers = Member.objects.all().values_list('first_name')
+    mymembers = Member.objects.filter(firstname='Emil').values()
     template = loader.get_template('template.html')
     context = {
         'mymembers': mymembers,
